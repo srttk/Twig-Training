19fb648d991eb8af3ab1b4d1aaf802f60b68082a
sarath <sarath.hacks@gmail.com>
2014-05-26 21:20:07 +0530

    Updated if-statement

--- src/Basics/if-statement.php
+++ src/Basics/if-statement.php
@@ -2,6 +2,9 @@
 include '../Twig_autoload.php';
 $loader=new Twig_Loader_Filesystem('templates'); //Setting Template folder
 $twig=new Twig_Environment($loader);
-
-echo $twig->render('if_statement.html',array('name'=>'sarath'));
+$name="sarath";
+if(isset($_GET['btn'])){
+	$name=$_GET['btn'];
+}
+echo $twig->render('if_statement.html',array('name'=>$name));
 
