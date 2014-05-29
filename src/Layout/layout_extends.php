37a29fa91331f67f1c6dc7d1ec72886789090243
sarath <sarath.hacks@gmail.com>
2014-05-29 20:49:56 +0530

    Changed version in README.md

--- src/Layout/layout_extends.php
+++ src/Layout/layout_extends.php
@@ -3,5 +3,5 @@ include '../Twig_autoload.php';
 $loader=new Twig_Loader_Filesystem('templates');
 $twig=new Twig_Environment($loader);
 
-$layout=$twig->loadTemplate('base.html');
-$twig->render('child.html');
+$template=$twig->loadTemplate('child.html');
+echo $template->render(array('name'=>'sarath'));
