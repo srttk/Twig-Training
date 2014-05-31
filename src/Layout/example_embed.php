296642940a8e39df564b64d705ce7f449657d4e8
sarath <sarath.hacks@gmail.com>
2014-05-31 09:50:51 +0530

    Started embed example

--- src/Layout/example_embed.php
+++ src/Layout/example_embed.php
@@ -3,5 +3,5 @@ include '../Twig_autoload.php';
 $loader=new Twig_Loader_Filesystem('templates');
 $twig=new Twig_Environment($loader);
 
-$template=$twig->loadTemplate('child.html');
+$template=$twig->loadTemplate('example_embed.html');
 echo $template->render(array('name'=>'sarath'));
