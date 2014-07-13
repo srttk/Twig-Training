c669cacab1b85dff0bd61921c36eb103b3fc5fae
sarath <sarath.hacks@gmail.com>
2014-07-13 20:28:16 +0530

    Added Variables Example file

---
+++ src/Basics/variables.php
@@ -0,0 +1,7 @@
+<?php
+require '../Twig_autoload.php';
+$loader=new Twig_Loader_Filesystem('templates');
+$twig=new Twig_Environment($loader);
+
+echo  $twig->render('variables.html',array('names'=>array('Sarath')));
+
