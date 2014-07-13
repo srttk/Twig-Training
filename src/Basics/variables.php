0c9b1271a7c6bdc594111d763903b7bc33227af2
sarath <sarath.hacks@gmail.com>
2014-07-13 20:46:05 +0530

    Updated variables.php

--- src/Basics/variables.php
+++ src/Basics/variables.php
@@ -3,5 +3,5 @@ require '../Twig_autoload.php';
 $loader=new Twig_Loader_Filesystem('templates');
 $twig=new Twig_Environment($loader);
 
-echo  $twig->render('variables.html',array('names'=>array('Sarath')));
+echo  $twig->render('variables.html',array('names'=>array('Sarath','Jeffrey'),'web'=>array('search'=>'Google','social-net'=>'Facebook')));
 
