782a50cacebbeddde2052c817c6f56fbf8d45d0a
sarath <sarath.hacks@gmail.com>
2014-07-19 17:56:10 +0530

    Some changes

--- src/Exaplepage/index.php
+++ src/Exaplepage/index.php
@@ -3,4 +3,9 @@ require '../Twig_autoload.php';
 
 $loader=new Twig_Loader_Filesystem('templates');
 $twig=new Twig_Environment($loader);
-echo $twig->render('home.html');
+
+$data=array(
+	'brand'=>'Stacketo',
+	'title'=>'Hello Universe'
+);
+echo $twig->render('home.html',$data);
